20751fda30f5fe87cc8f2cc5a7c42f8f48e69e2e
007Alena <alena7846@mail.ru>
2023-02-24 23:08:58 +0300

    fixed bugs + router

--- src/components/Header/About/About.jsx
+++ src/components/Header/About/About.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { ArrowRightOutlined } from "@ant-design/icons";
 
-const About = () => {
+function About({ activeTest }) {
     return (
         <div className="header-about sizeblock">
             <div className="header-about__text">
                 <div className="header-about__text-slogan"><span className="header-about__text-name">Школа <span className="header-about__text-y">Y</span>t<span className="header-about__text-y2">Y</span>t</span> для тех, кому нужны реальные навыки,
                     а не просто сертификат</div>
                 <div className="header-about__text-test">Пройдите тестирование, чтобы получить доступ к бесплатным вводным урокам</div>
-                <div className="header-about__text-button button">Пройти тестирование
+                <div className="header-about__text-button button" onClick={() => { activeTest(true) }}>Пройти тестирование
                     <span className="header-about__text-buttonarrow arrow"><ArrowRightOutlined /></span>
                 </div>
             </div>
