6a1021c596a23693154d7512b6f3c1dc972b06af
007Alena <alena7846@mail.ru>
2023-03-13 02:52:12 +0300

    form reg,log and user page

--- src/App.js
+++ src/App.js
@@ -1,30 +1,16 @@
-import { useState } from 'react';
 import './App.css';
 import './components/Assets/Styles/base.scss'
-import Header from './components/Header/header';
-import Main from './components/Main/Main';
-import Footer from './components/Footer/Footer';
-import ITtest from './components/Test/Test';
-import LogIn from './components/Header/LogIn/LogIn';
-import RegistrationForm from './components/Header/RegistrationForm/RegistrationForm';
-import {
-  BrowserRouter,
-} from "react-router-dom";
+import MainPage from './components/MainPage/MainPage';
+import UserPage from './components/UserPage/UserPage';
+import {Routes, Route} from "react-router-dom"
 
 function App() {
-  const [Tests, setTest] = useState(false);
-  const [Registration, setRegistration] = useState(false);
-  const [LogIntrue, setLogIntrue] = useState(false)
   return (
     <>
-      <BrowserRouter>
-      {Registration && <RegistrationForm closeTest = {setRegistration}/>}
-      {LogIntrue && <LogIn closeTest = {setLogIntrue}/>}
-      {Tests && <ITtest closeTest = {setTest}/>}
-      <Header activeTest = {setTest} activereg ={setRegistration} activelog={setLogIntrue}/>
-      <Main activeTest = {setTest}/>
-      </BrowserRouter>
-      <Footer/>
+      <Routes>
+      <Route extra path='/user' element={<UserPage />} />
+      <Route extra path='/' element={<MainPage />} />
+      </Routes>
     </>
   );
 }
---
+++ src/components/Elements/Header/Menu/BurgerMenu.jsx
@@ -0,0 +1,40 @@
+import { CloseOutlined, ArrowRightOutlined } from "@ant-design/icons";
+import { HashLink } from 'react-router-hash-link';
+function BurgerMenu({ active, setActive, activereg }) {
+    return (
+        <div className={active ? 'menu active' : 'menu'}>
+            <div className='blur' onClick={() => setActive(false)}>
+                <div className='menu-content'>
+                    <div className='menu-content__close'>
+                        <CloseOutlined onClick={() => setActive(false)} />
+                    </div>
+                    <div className="menu-content__position">
+                        <ul className='menu-content__block'>
+                            <li onClick={() => setActive(false)} >
+                                <HashLink to="../../Main/Programma/Programma#main-programma" className='menu-content__point'>Чему вы научитесь</HashLink>
+                            </li>
+                            <li onClick={() => setActive(false)} >
+                                <HashLink to="/../../Main/Price/PriceAndRules#main-howstudy" className='menu-content__point'>Процесс обучения</HashLink>
+                            </li>
+                            <li onClick={() => setActive(false)} >
+                                <HashLink to="../../Main/Price/PriceAndRules#mainprice-table" className='menu-content__point'>Стоимость</HashLink>
+                            </li>
+                            <li onClick={() => setActive(false)} >
+                                <HashLink to="../../Footer/Footer#footer" className='menu-content__point'>Контакты</HashLink>
+                            </li>
+                        </ul>
+                        <div onClick={() => setActive(false)} >
+                            <p className='menu-content__point' onClick={() => { activereg(true) }}>Регистрация</p>
+                        </div>
+                        <div className="menu-content__burgerlogin login-button">
+                            <span className="enu-content__burgerspanlogin login-arrow"><ArrowRightOutlined /></span>
+                            Войти
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default BurgerMenu;
